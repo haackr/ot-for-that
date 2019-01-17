6beeea3390791615bd05bd3f1b09ecec7736cffc
Ryan Haack <ryan@haackr.com>
2019-01-17 16:51:19 -0700

    Add form-name input for netlify form handling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,15 +8,16 @@ const Contact = ({location}) => (
     <SEO title="OT for That - Contact Me" />
     <div id="contact-form">
       <h1>Contact Me:</h1>
-      <form method="POST" name="contact" data-netlify="true">
-          {/* <div className="hidden"><label>Are you human? <input name="bot-field" /></label></div> */}
-          <label htmlFor="name">Your Name:</label>
-          <input type="text" name="name" required/>
-          <label htmlFor="email">Your Email:</label>
-          <input type="text" name="email" required/>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="15" required></textarea>
-          <button type="submit">Send</button>
+      <form method="POST" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact"/>
+        <div className="hidden"><label>Are you human? <input name="bot-field" /></label></div>
+        <label htmlFor="name">Your Name:</label>
+        <input type="text" name="name" required/>
+        <label htmlFor="email">Your Email:</label>
+        <input type="text" name="email" required/>
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" rows="15" required></textarea>
+        <button type="submit">Send</button>
       </form>
     </div>
   </Layout>
