4cf4bd9461d8f57949aa04016b15c7eaf29c6aaf
Ryan Haack <ryan@haackr.com>
2019-01-17 16:36:30 -0700

    Debugging contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,8 +8,8 @@ const Contact = ({location}) => (
     <SEO title="OT for That - Contact Me" />
     <div id="contact-form">
       <h1>Contact Me:</h1>
-      <form method="POST" name="contact" data-netlify="true" action="/">
-          <div className="hidden"><label>Are you human? <input name="bot-field" /></label></div>
+      <form method="POST" name="contact" data-netlify="true">
+          {/* <div className="hidden"><label>Are you human? <input name="bot-field" /></label></div> */}
           <label htmlFor="name">Your Name:</label>
           <input type="text" name="name" required/>
           <label htmlFor="email">Your Email:</label>
